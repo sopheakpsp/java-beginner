c7a8e9f11bf5d3ed3985d1062a8375895812176d
So Pheak <sopheak.psp@gmail.com>
2017-02-13 17:33:26 +0700

    OK

--- src/basic/code/JumpBreak.java
+++ src/basic/code/JumpBreak.java
@@ -1,11 +1,17 @@
 package basic.code;
 
 public class JumpBreak {
-
+	static int n = 20, i = 0;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n = 20, i = 0;
-		
+//		BreakFor();
+		BreakWhile();
+//		BreakDo();
+	}
+
+
+	public static void BreakFor(){
 		for(i=0; i<n; i++){
 			if(i==9) 
 				break;
@@ -13,7 +19,9 @@ public class JumpBreak {
 		}
 		System.out.println();
 		System.out.println("This statement is out of for loop");
-		
+	}
+	
+	public static void BreakWhile(){
 		while(i<n){
 			if(i==9)
 				break;
@@ -22,14 +30,16 @@ public class JumpBreak {
 		}
 		System.out.println(i);
 		System.out.println("This statement is out of while loop");
-		
+	}
+	
+	public static void BreakDo(){
 		do{
 			System.out.print(i + " ");
 			i++;
+			if(i==8)
+				break;
 		}while(i<n);
 		System.out.println();
 		System.out.println("This statement is out of do while");
-		
 	}
-
 }
